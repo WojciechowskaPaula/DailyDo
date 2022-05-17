2ad2b2818d9b4947b687d5701c44639f95503fcd
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-05-17 13:12:12 +0200

    Add Task model and DbContext

---
+++ DailyDo/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DailyDo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base (options) { }
+        DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ DailyDo/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyDo.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Display(Name = "Modification Date")]
+        public DateTime ModificationDate { get; set; }
+
+        [Required]
+        public bool IsDone { get; set; }
+    }
+}
