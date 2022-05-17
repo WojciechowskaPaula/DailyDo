99b7c729c909fced8ec3383e8152c632976845c4
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-05-17 15:11:26 +0200

    Change ConnectionString

--- DailyDo/Program.cs
+++ DailyDo/Program.cs
@@ -1,7 +1,12 @@
+using DailyDo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
 var app = builder.Build();
 
