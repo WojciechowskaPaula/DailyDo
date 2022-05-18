94d9b2044594dea6141bed6e1e6fd932ec13e70e
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-05-18 22:25:18 +0200

    Adding display all tasks and AddForm

---
+++ DailyDo/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using DailyDo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyDo.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public TaskController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var allTasks = _dbContext.Tasks.ToList();
+            return View(allTasks);
+        }
+
+        [HttpGet]
+        public IActionResult AddNewForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddNew(DailyDo.Models.Task task)
+        {
+           var taskToAdd = _dbContext.Tasks.Add(task);
+            task.ModificationDate = DateTime.Now;
+            task.IsDone = false;
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
