d7dd157377fc852dd375a03a5127d20ac169eca3
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-05-26 15:50:25 +0200

    Adding TaskAndCategoryListsVM

---
+++ DailyDo/Models/TaskAndCategoryListsVM.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyDo.Models
+{
+    public class TaskAndCategoryListsVM
+    {
+        public List<Task> Tasks { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
