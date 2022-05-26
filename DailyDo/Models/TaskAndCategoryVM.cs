b528dcb380df278a76a8d9b7814b3ef64137ec68
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-05-26 10:22:39 +0200

    Create TaskAndCategoryVM

---
+++ DailyDo/Models/TaskAndCategoryVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyDo.Models
+{
+    public class TaskAndCategoryVM
+    {
+       public Task Task { get; set; }
+
+       public List<Category> Categories { get; set; }
+    }
+}
