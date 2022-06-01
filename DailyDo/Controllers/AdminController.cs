a947fc8d846bbf060d2097772b244826fe85973d
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-01 15:41:19 +0200

    Creating a AdminController and providing the ability to set up authorization roles

---
+++ DailyDo/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using DailyDo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyDo.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddNewRoleForm()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> AddNewRole(ProjectRoleVM projectRole)
+        {
+            var roleExist = await _roleManager.RoleExistsAsync(projectRole.RoleName);
+            if (!roleExist)
+            {
+                var result = await _roleManager.CreateAsync(new IdentityRole(projectRole.RoleName));
+            }
+            return View();
+        }
+    }
+}
