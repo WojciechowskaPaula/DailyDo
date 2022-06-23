ee67c41b68a440808bf2dd16e475494c4fd963d1
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-23 16:08:55 +0200

    Adding logs in AdminController

--- DailyDo/Controllers/AdminController.cs
+++ DailyDo/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace DailyDo.Controllers
 {
@@ -9,33 +10,48 @@ namespace DailyDo.Controllers
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<AdminController> _logger;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, ILogger<AdminController> logger)
         {
             _roleManager = roleManager;
+            _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            _logger.LogInformation("action=adminIndex");
             return View();
         }
 
         [HttpGet]
         public IActionResult AddNewRoleForm()
         {
-           
+            _logger.LogInformation("action=AddNewRoleForm");
             return View();
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task <IActionResult> AddNewRole(ProjectRoleVM projectRole)
         {
-            var roleExist = await _roleManager.RoleExistsAsync(projectRole.RoleName);
-            if (!roleExist)
+            try
             {
-                var result = await _roleManager.CreateAsync(new IdentityRole(projectRole.RoleName));
+                _logger.LogInformation($"action=AddNewRole projectRole={JsonSerializer.Serialize(projectRole)}");
+                var roleExist = await _roleManager.RoleExistsAsync(projectRole.RoleName);
+                if (!roleExist)
+                {
+                    var result = await _roleManager.CreateAsync(new IdentityRole(projectRole.RoleName));
+                }
+                _logger.LogInformation($"action=AddNewRole msg='A new role have been saved'");
+                return View();
             }
-            return View();
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=AddNewRole msg='{ex.Message}' projectRole={JsonSerializer.Serialize(projectRole)}", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
