ff9157653843d1324a6aff7af02983603ad6707c
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-23 15:41:51 +0200

    Adding try/catch to TaskController

--- DailyDo/Controllers/TaskController.cs
+++ DailyDo/Controllers/TaskController.cs
@@ -21,104 +21,154 @@ namespace DailyDo.Controllers
         }
 
         [HttpGet]
-        public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
-            _logger.LogInformation("action=index");
-            var signInUser = await _userManager.GetUserAsync(HttpContext.User);
-
-            var allTasks = _dbContext.Tasks.Where(x => x.User == signInUser).ToList();
-            var categories = _dbContext.Categories.ToList();
-            _logger.LogInformation($"action=index taskCount={allTasks.Count}, categoriesCount={categories.Count}");
-            var listsVM = new TaskAndCategoryListsVM();
-            listsVM.Tasks = allTasks;
-            listsVM.Categories = categories;
-            return View(listsVM);
+            try
+            {
+                _logger.LogInformation("action=index");
+                var signInUser = await _userManager.GetUserAsync(HttpContext.User);
+                var allTasks = _dbContext.Tasks.Where(x => x.User == signInUser).ToList();
+                var categories = _dbContext.Categories.ToList();
+                _logger.LogInformation($"action=index taskCount={allTasks.Count}, categoriesCount={categories.Count}");
+                var listsVM = new TaskAndCategoryListsVM();
+                listsVM.Tasks = allTasks;
+                listsVM.Categories = categories;
+                return View(listsVM);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=index msg='{ex.Message}'", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpGet]
         public IActionResult AddNewForm()
         {
-            _logger.LogInformation("action=addNewForm");
-            var categories = _dbContext.Categories.ToList();
-            _logger.LogInformation($"action=addNewForm categoriesCount={categories.Count}");
-            TaskAndCategoryVM taskAndCategory = new TaskAndCategoryVM();
-            taskAndCategory.Categories = categories;
-            
-            return View(taskAndCategory);
+            try
+            {
+                _logger.LogInformation("action=addNewForm");
+                var categories = _dbContext.Categories.ToList();
+                _logger.LogInformation($"action=addNewForm categoriesCount={categories.Count}");
+                TaskAndCategoryVM taskAndCategory = new TaskAndCategoryVM();
+                taskAndCategory.Categories = categories;
+                return View(taskAndCategory);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=addNewForm msg='{ex.Message}'", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task <IActionResult> AddNew(TaskAndCategoryVM taskAndCategoryVM)
+        public async Task<IActionResult> AddNew(TaskAndCategoryVM taskAndCategoryVM)
         {
-
-            _logger.LogInformation($"action=addNew task='{JsonSerializer.Serialize(taskAndCategoryVM)}'");
-            
-            var taskFromVM = taskAndCategoryVM.Task;
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-            var categoryFromVM = _dbContext.Categories.Where(x => x.CategoryId == taskAndCategoryVM.Task.Category.CategoryId).FirstOrDefault();
-            taskFromVM.Category = categoryFromVM;
-            taskFromVM.ModificationDate = DateTime.Now;
-            taskFromVM.IsDone = false;
-            taskFromVM.User = user;
-            var taskToAdd = _dbContext.Tasks.Add(taskFromVM);
-            _dbContext.SaveChanges();
-            _logger.LogInformation("action=addNew msg='the task saved'");
-            return RedirectToAction("Index");
+            try
+            {
+                _logger.LogInformation($"action=addNew task='{JsonSerializer.Serialize(taskAndCategoryVM)}'");
+                var taskFromVM = taskAndCategoryVM.Task;
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                var categoryFromVM = _dbContext.Categories.Where(x => x.CategoryId == taskAndCategoryVM.Task.Category.CategoryId).FirstOrDefault();
+                taskFromVM.Category = categoryFromVM;
+                taskFromVM.ModificationDate = DateTime.Now;
+                taskFromVM.IsDone = false;
+                taskFromVM.User = user;
+                var taskToAdd = _dbContext.Tasks.Add(taskFromVM);
+                _dbContext.SaveChanges();
+                _logger.LogInformation("action=addNew msg='the task saved'");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=addNew msg='{ex.Message}' task='{JsonSerializer.Serialize(taskAndCategoryVM)}'", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpGet]
         public IActionResult EditForm(int id)
         {
-            _logger.LogInformation($"action=editForm id={id}");
-            var taskToEdit = _dbContext.Tasks.Where(x => x.Id == id).FirstOrDefault();
-            _logger.LogInformation($"action=editForm msg='the task with id:{id} was found'");
-            return View(taskToEdit);
+            try
+            {
+                _logger.LogInformation($"action=editForm id={id}");
+                var taskToEdit = _dbContext.Tasks.Where(x => x.Id == id).FirstOrDefault();
+                _logger.LogInformation($"action=editForm msg='the task with id:{id} was found'");
+                return View(taskToEdit);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=editForm msg='{ex.Message}' id='{id}'", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Update(DailyDo.Models.Task task)
         {
-            _logger.LogInformation($"action=update task='{JsonSerializer.Serialize(task)}'");
-            var updateTask = _dbContext.Tasks.Where(x => x.Id == task.Id);
-            task.ModificationDate = DateTime.Now;
-            task.IsDone = false;
-            _dbContext.Entry(task).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _dbContext.SaveChanges();
-            _logger.LogInformation("action=update msg='the task was modified'");
-            return RedirectToAction("Index");
+            try
+            {
+                _logger.LogInformation($"action=update task='{JsonSerializer.Serialize(task)}'");
+                var updateTask = _dbContext.Tasks.Where(x => x.Id == task.Id);
+                task.ModificationDate = DateTime.Now;
+                task.IsDone = false;
+                _dbContext.Entry(task).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _dbContext.SaveChanges();
+                _logger.LogInformation("action=update msg='the task was modified'");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=update msg='{ex.Message}' task='{JsonSerializer.Serialize(task)}'", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
-
 
         public IActionResult Delete(int id)
         {
-            _logger.LogInformation($"action=delete id={id}");
-            var taskToDelete = _dbContext.Tasks.Where(x => x.Id == id).FirstOrDefault();
-            _dbContext.Remove(taskToDelete);
-            _dbContext.SaveChanges();
-            _logger.LogInformation($"action=delete msg='task with id:{id} has been removed'");
-            return RedirectToAction("Index");
+            try
+            {
+                _logger.LogInformation($"action=delete id={id}");
+                var taskToDelete = _dbContext.Tasks.Where(x => x.Id == id).FirstOrDefault();
+                _dbContext.Remove(taskToDelete);
+                _dbContext.SaveChanges();
+                _logger.LogInformation($"action=delete msg='task with id:{id} has been removed'");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=delete msg='{ex.Message}' id={id}", ex);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult SetDone([FromBody] DailyDo.Models.Task task)
         {
-            _logger.LogInformation($"action=setDone task='{JsonSerializer.Serialize(task)}'");
-            var taskToCheck = _dbContext.Tasks.Where(x => x.Id == task.Id).FirstOrDefault();
-            if (taskToCheck.IsDone)
+            try
             {
-                taskToCheck.IsDone = false;
+                _logger.LogInformation($"action=setDone task='{JsonSerializer.Serialize(task)}'");
+                var taskToCheck = _dbContext.Tasks.Where(x => x.Id == task.Id).FirstOrDefault();
+                if (taskToCheck.IsDone)
+                {
+                    taskToCheck.IsDone = false;
+                }
+                else
+                {
+                    taskToCheck.IsDone = true;
+                }
+                _dbContext.SaveChanges();
+                _logger.LogInformation($"action=index msg='the task isDone:{task.IsDone} was modified'");
+                return Ok(true);
             }
-            else
+            catch(Exception ex)
             {
-                taskToCheck.IsDone = true;
+                _logger.LogError($"action=setDone msg='{ex.Message}' task='{JsonSerializer.Serialize(task)}'", ex);
+                return RedirectToAction("Error","Home");
             }
-            _dbContext.SaveChanges();
-            _logger.LogInformation($"action=index msg='the task isDone:{task.IsDone} was modified'");
-            return Ok(true);
         }
-
     }
 }
