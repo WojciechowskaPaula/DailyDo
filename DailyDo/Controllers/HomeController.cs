0003d1cfe3e1b2db643738c174576b73a10d1e93
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-23 15:13:30 +0200

    Remove unused method Privacy and changing Error view

--- DailyDo/Controllers/HomeController.cs
+++ DailyDo/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace DailyDo.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
